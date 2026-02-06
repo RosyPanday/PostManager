51321cdeae86aa9cadbcd90e7f1065588865ee74
RosyPanday <pandayrosy14@gmail.com>
2026-02-06 14:30:58 +0545

    feat:connection to database done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 //need type definitions for express so, install a dev dependency @types/express
-import dotenv from 'dotenv';
+import dotenv from 'dotenv';  
+import 'dotenv/config'; // this loads the variables first to reduce redundancy and hositing occurs
 
 //express is written in plain js , so this will be a problem
 
-dotenv.config();  //loading the variables
 const app=express();
+import { sequelize } from './database/connection.js';  
 
-
-const port= Number(process.env.PORT)|| 3000;// fallbak 3000,if someone forgets to inject to .env
+const port= Number(process.env.PORT)|| 3000;// fallback 3000,if someone forgets to inject to .env
 
 // app.use(express.json());
 
---
+++ src/database/connection.ts
@@ -0,0 +1,21 @@
+import {Sequelize} from 'sequelize';
+ const CONNECTION_STRING= process.env.CONNECTION_STRING;
+
+ if(!CONNECTION_STRING){
+    throw new Error("undefined connection string");
+ } 
+    export const sequelize:Sequelize= new Sequelize(CONNECTION_STRING);
+ 
+ const connectDb = async():Promise<void> =>{
+    try{
+     await sequelize.authenticate();
+     console.log("authenticated");
+    } catch(err :unknown) {
+
+        if(err instanceof Error){
+         console.log(err.message);
+        }
+    }
+ }
+
+ connectDb();
